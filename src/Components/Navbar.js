6e61e16147fdbdbdd0469a28805af3d915bea5cd
t4shar <tushar.gandhi0092@gmail.com>
2021-11-25 23:07:32 +0530

    Online-Shorts

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react'
+import React, { useState } from 'react'
 
 import {
   Link
@@ -27,7 +27,7 @@ const Navbar = (props)=> {
         
         <nav className={state.mode}>
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">One Liner</Link>
+            <Link className="navbar-brand" to="/">Online-Shots</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
